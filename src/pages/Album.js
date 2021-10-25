e5f3ae388f9c7eeed7e2524fc2c1db0646636d72
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-10-25 09:15:03 -0300

    welckson valentim - Finaliza requisito 8, apos aprovaçao local

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class Album extends React.Component {
   constructor() {
     super();
     this.state = {
+      loadFavoritesong: '',
     };
   }
 
@@ -20,7 +22,7 @@ class Album extends React.Component {
       const idAlbum = URLAlbum.substring(startURL, endURL);
       const chanceToString = String(idAlbum);
       const albumInfo = await getMusics(chanceToString);
-      if (albumInfo.length !== 0) {
+      if (albumInfo !== undefined) {
         this.setState({
           nameArtist: albumInfo[0].artistName,
           nameAlbum: albumInfo[0].collectionName,
@@ -29,34 +31,62 @@ class Album extends React.Component {
       }
     }
 
+    checkFavoriteMusic = async () => {
+      const { allInformation } = this.state;
+      this.setState({
+        loadFavoritesong: 'Carregando...',
+      });
+      await addSong(allInformation);
+      this.setState({
+        loadFavoritesong: '',
+      });
+    }
+
     render() {
-      const { nameArtist, nameAlbum, allInformation } = this.state;
+      const { nameArtist, nameAlbum, allInformation, loadFavoritesong } = this.state;
       return (
         <div
           data-testid="page-album"
+          key={ nameArtist }
         >
           <Header />
-          {nameArtist !== undefined && (
+          {nameArtist !== undefined ? (
             <div
-              key="1"
+              key={ nameArtist }
             >
               <h3 key="artistName" data-testid="artist-name">{nameArtist}</h3>
               <h3 key="nameAlbum" data-testid="album-name">{nameAlbum}</h3>
-            </div>)}
+            </div>) : 'name of artist undefined'}
 
           {allInformation !== undefined && allInformation.map((track) => (
             track.previewUrl !== undefined && (
-              <audio
-                data-testid="audio-component"
-                src={ track.previewUrl }
-                key={ track.previewUrl }
-                controls
-              >
-                <track kind="captions" />
-                O seu navegador não suporta o elemento
-                <code>audio</code>
-                <h4>{ track.trackName }</h4>
-              </audio>)))}
+              <div>
+                <label
+                  htmlFor={ track.trackId }
+                  key={ track.trackId }
+                >
+                  Favorita
+                  <input
+                    data-testid={ `checkbox-music-${track.trackId}` }
+                    type="checkbox"
+                    onClick={ this.checkFavoriteMusic }
+                  />
+                </label>
+                <audio
+                  data-testid="audio-component"
+                  src={ track.previewUrl }
+                  key={ track.previewUrl }
+                  controls
+                >
+                  <track kind="captions" />
+                  O seu navegador não suporta o elemento
+                  <code>audio</code>
+                  <h4>{ track.trackName }</h4>
+                </audio>
+                )
+              </div>
+            )))}
+          <h4>{loadFavoritesong}</h4>
         </div>
       );
     }
